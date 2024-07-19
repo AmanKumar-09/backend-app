9002de9fa0447689258f4d01a6f34aea04aac63e
AmanKumar-9 <aak420573@gmail.com>
2024-07-19 15:57:45 +0530

    Added watch history

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -2,7 +2,7 @@ import mongoose, { Schema } from "mongoose";
 
 const subscriptionSchema = new Schema(
   {
-    Subscription: {
+    subscriber: {
       type: Schema.Types.ObjectId, // one who is subscribing
       ref: "User",
     },
